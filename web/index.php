3320faea141f6866e9173abc22f7d52ee8e1b2ee
tanthoine <tanthoine@hi-media.com>
2014-05-22 18:29:31 +0200

    Improve silex implementation

--- web/index.php
+++ web/index.php
@@ -8,11 +8,25 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Provider\ValidatorServiceProvider());
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/views'
+$app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../app/config/settings.yml'));
+$app->register(new Silex\Provider\SwiftmailerServiceProvider(), array(
+    'swiftmailer.options' => array(
+        'host'       => $app['config']['swiftmailer']['host'],
+        'port'       => $app['config']['swiftmailer']['port'],
+        'username'   => $app['config']['swiftmailer']['username'],
+        'password'   => $app['config']['swiftmailer']['password'],
+        'encryption' => $app['config']['swiftmailer']['encryption'],
+        'auth_mode'  => $app['config']['swiftmailer']['auth_mode']
+    )
 ));
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views'
+));
+
+$app['mailer'] = $app->share(function ($app) {
+    return new \Swift_Mailer($app['swiftmailer.transport']);
+});
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('index.html', array());
@@ -41,17 +55,22 @@ $app->post('/mail', function() use ($app) {
         return $app->json(array('message' => implode('<br />', $errorMessages)), 404);
     }
 
-    $message = \Swift_Message::newInstance()
-        ->setSubject(sprintf(
-            '[thibaudanthoine.com][%s] %s',
-            $app['request']->get('name'),
-            $app['request']->get('subject')
-        ))
-        ->setFrom(array($app['request']->get('email')))
-        ->setTo(array('thibaud@groovinmove.com'))
-        ->setBody($app['request']->get('message'));
+    try {
+        $message = \Swift_Message::newInstance()
+            ->setSubject(sprintf(
+                '[thibaudanthoine.com][%s] %s',
+                $app['request']->get('name'),
+                $app['request']->get('subject')
+            ))
+            ->setFrom(array($app['request']->get('email')))
+            ->setTo(array('thibaud@groovinmove.com'))
+            ->setBody($app['request']->get('message'));
 
-    $app['mailer']->send($message);
+        $app['mailer']->send($message);
+    }
+    catch (\Exception $e) {
+        return $app->json(array('message' => 'Sorry, unable to send email'), 404);
+    }
 
     return $app->json(array('message' => 'Thank you for your message'));
 });
